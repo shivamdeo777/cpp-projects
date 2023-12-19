8df949665713b812d606363d4471b59b23d7b38b
shivamdeo777 <154303337+shivamdeo777@users.noreply.github.com>
2023-12-19 11:13:28 +0530

    CPP program first

---
+++ nth_turn.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,sum;
+    cout<<"enter number : ";
+    cin>>n;
+    sum=n*(n+1)/2;
+    cout<<"sum of term : "<<sum;
+    return 0;
+}
---
+++ triangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int b,h,area;
+    cout<<"enter base and height";
+    cin>>b>>h;
+    area=(b*h)/2;
+    cout<<"area of traingle is"<<area;
+    return 0;
+}
